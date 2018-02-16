d5fb1013cbc8ce6eb40437aa0aea5f830f6d0dc4
hwisnik <hwisnik@hotmail.com>
2018-02-16 16:28:44 -0500

    Add project files.

---
+++ Logger/EntLogger.cs
@@ -0,0 +1,24 @@
+﻿using log4net;
+using System;
+
+namespace EnterpriseLogger
+{
+    public static class EntLogger
+    {
+        private static readonly ILog Log = LogManager.GetLogger(typeof(EntLogger));
+
+        public static ILog LoggingInstance
+        {
+            get { return Log; }
+        }
+
+        static EntLogger()
+        {
+            GlobalContext.Properties["ApplicationName"] = AppDomain.CurrentDomain.FriendlyName;
+            log4net.Config.XmlConfigurator.Configure();
+        }
+
+        //TODO:  Need to implement a correlationId so that you can group multiple logfile entries
+    }
+}
+
---
+++ LoggerTest/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using EnterpriseLogger;
+using log4net;
+
+namespace LoggerTest
+{
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var logger = new LoggerTest();
+            logger.Logit();
+        }
+
+
+
+        public class LoggerTest
+        {
+            private readonly ILog _log = EntLogger.LoggingInstance;
+            public void Logit()
+            {
+                try
+                {
+                    throw new Exception("Test Message Text",new Exception("Inner Exception Text"));
+                }
+                catch (Exception ex)
+                {
+                    _log.Error(ex.Message,ex);
+                }
+            }
+        }
+    }
+}
